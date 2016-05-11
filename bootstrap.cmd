db097a8364b6f54200e649284ea1aa97c847ed5c
Paul Welter <pwelter@loresoft.com>
2016-05-11 13:45:36 -0500

    add build script

---
+++ bootstrap.cmd
@@ -0,0 +1,5 @@
+@echo off
+Nuget.exe restore "Source\EntityChange.sln"
+
+NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
+NuGet.exe install xunit.runner.console -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
