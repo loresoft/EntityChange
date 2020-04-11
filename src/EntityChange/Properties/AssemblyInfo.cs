09b3e9270560ede4269d1f0dc749deecd2f210d6
Paul Welter <pwelter@loresoft.com>
2020-04-11 11:17:32 -0500

    update build

--- src/EntityChange/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EntityChange")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("0258caa6-11bc-4a8b-84ae-ab31af240627")]
